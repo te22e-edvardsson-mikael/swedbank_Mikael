cf658f4dde370d71cd35eadd056e580350a1ffde
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2025-03-09 01:22:08 +0100

    Experimentarar med att skapa kund

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,27 @@ public class Main {
 
 
         ArrayList<Kund> kunder = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Välkommen till swedbank AB?");
+
+            System.out.println("Ange namn för konto: ");
+
+            String namn = scanner.nextLine();
+
+            System.out.println("Ange ditt kund-ID; ");
+
+            int kundID = scanner.nextInt();
+
+            System.out.println("Ange personnummer: ");
+
+            String personnummer = scanner.nextLine();
+
+            kunder.add(new Kund(namn, kundID, personnummer));
+
+
+
+        }
     }
 }
