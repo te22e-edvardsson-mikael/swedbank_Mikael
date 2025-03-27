e2fd6d90cfb73eb8e482eb9f87893c59abb520c6
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2025-03-27 10:26:28 +0100

    fixade kontor och gjorde getters för den

--- src/Kontor.java
+++ src/Kontor.java
@@ -1,4 +1,17 @@
 public class Kontor {
-    private String Adress;
+    private String adress;
     private String oppettider;
+
+    public Kontor(String adress, String oppettider){
+        this.adress = adress;
+        this.oppettider = oppettider;
+
+    }
+
+    public String getAdress(){
+        return adress;
+    }
+    public String getOppettider(){
+        return oppettider;
+    }
 }
