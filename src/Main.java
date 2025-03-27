3a3195debcd34cef3ebe2dd01545b82d49295f39
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2025-03-27 10:40:45 +0100

    kan man ta ut lån

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,47 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*public class Main {
+public class Main {
     public static void main(String[] args) {
 
 
         ArrayList<Kund> kunder = new ArrayList<>();
-        ArrayList<Konto> konton = new ArrayList<>();
-
-
-
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Du visiterar Swedbank");
+
+
+
         while(true){
-            System.out.println("Välkommen till swedbank AB?");
 
             System.out.println("Ange namn för konto: ");
-
             String namn = scanner.nextLine();
 
             System.out.println("Ange ditt kund-ID; ");
-
             int kundID = scanner.nextInt();
             scanner.nextLine(); // behöver vara där annars blir det något loop typ
 
             System.out.println("Ange personnummer: ");
-
             String personnummer = scanner.nextLine();
 
 
             //ny kund i listan med sin egna unikheter
             Kund nyKund = new Kund(namn, kundID, personnummer);
             kunder.add(nyKund);
+            System.out.println("Kund registerad!");
 
-            System.out.println("hur mycket saldo vill du lägga till i konto med kundID" + kundID + "?");
-            float saldo = scanner.nextFloat();
-            scanner.nextLine(); // behöver vara där annars blir det något loop typ
+            System.out.println("vill du ta ett lån (ja/nej)");
+            String svar = scanner.nextLine();
+            if (svar.equalsIgnoreCase("ja")){
+                System.out.println("ange lånbelopp: ");
+                float lanebelopp = scanner.nextFloat();
+                scanner.nextLine();
+                nyKund.laggTillLan(lanebelopp);
+                System.out.println("tagit lån: " + lanebelopp);
+            }
 
 
-            String kontonummer = "konto: " + kundID;
-            Konto nyttkonto = new Konto(kontonummer, saldo);
-            konton.add(nyttkonto);
 
-            System.out.println("Konto informationen för kund med ID" + nyKund.getKundID() + "samt personnummer " + nyKund.getPersonnummer());
-            System.out.println("Namn: " + nyKund.getName());
-            System.out.println("Saldo: " + nyttkonto.getSaldo());
 
             break; //avsluta while loopen
 
